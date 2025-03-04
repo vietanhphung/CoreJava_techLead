0e3ee2555cef74e0a82c773fbf460041f9e51178
Viet Anh (Connor) Phung <pvaconnor@gmail.com>
2025-03-04 09:29:06 +0700

    clean repo

--- sql_spring copy/src/main/java/com/java_sql/main/exception/DuplicateUserException.java
+++
@@ -1,7 +0,0 @@
-package com.java_sql.main.exception;
-
-public class DuplicateUserException extends RuntimeException {
-    public DuplicateUserException(String message) {
-        super(message);
-    }
-}
